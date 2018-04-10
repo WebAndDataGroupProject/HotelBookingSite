da1bcd3183d0d7c3a86b3c886e9ea1e116844a30
Renee Marsland <reneemarsland@hotmail.com>
2018-04-10 13:46:45 +0930

    Adding files and home changes
    adding AccountSettings.html and trying to get it to load into content. Doesnt appear to be working.

--- Part2/home.js
+++
@@ -1,6 +0,0 @@
-
-// Load the Account Page html into the homepage.
-function account_page()
-{
-		document.getElementById("content").innerHTML = '<object type= "text/html" data= "accountPage.html"></object>';
-}
