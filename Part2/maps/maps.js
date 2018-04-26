73c2e8409e04e40b5b26f8f1ad5399bcc3ce292d
Renee Marsland <reneemarsland@hotmail.com>
2018-04-26 12:40:05 +0930

    Trying to load map markers from xml
    I can't get the ajax method in the maps.js file to work, right now it is erroring every time.

--- Part2/maps/maps.js
+++ Part2/maps/maps.js
@@ -2,14 +2,45 @@
 
 // Global variables
 var map = null;
-var placeHolder = {lat: -34.920, lng: 138.606};
+var centre = {lat: -34.920, lng: 138.606};
 
 function initMap() {
 	var mapOptions = {
-		center: placeHolder,
+		center: centre,
 		zoom : 12,
 		styles: [ {visibility: "on"} ]
 	};
 
 	map = new google.maps.Map(document.getElementById("map"), mapOptions);
+	console.log("map loaded");
+
+	// CAN'T GET AJAX METHOD TO SUCCEED...
+	// Load the XML file.
+	$.ajax({
+		type: "GET",
+		async: true,
+		url: "markers.xml",
+		dataType: "xml",
+		success:
+		function (xml) {
+			// An array of places.
+			var places = xml.documentElement.getElementsByTagName("place");
+
+			// Loops though each marker and adds it to the map.
+			for (var i = 0; i < places.length; i++) {
+				var lat = places[i].getAttribute('latitude');
+				var long = places[i].getAttribute('longitude');
+				var latLng = new google.maps.LatLng(lat, long);
+				var marker = new google.maps.Marker({
+				position:  latLng,
+				map: map,
+				label:places[i].name
+				});
+			}
+		},
+		error:
+		function() {
+			console.log("AJAX FAILED");
+		}
+	});
 }
