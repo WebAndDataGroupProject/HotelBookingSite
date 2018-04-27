c9a5cca0e027c0daf2ebc80433dde82f128655aa
Renee Marsland <reneemarsland@hotmail.com>
2018-04-27 17:42:41 +0930

    added static google maps function
    now loads static hotel markers on map pop-up.

--- public/javascripts/maps/maps.js
+++ public/javascripts/maps/maps.js
@@ -4,7 +4,15 @@
 var map = null;
 var centre = {lat: -34.920, lng: 138.606};
 
-// Show all hotels on one map.
+// Static markers
+var mayfairMarker;
+var ibisMarker;
+
+var ibisLatLng = {lat: -34.923870, lng:138.604218};
+var mayfairLatLng = {lat: -34.923271, lng:138.599176};
+
+
+// Initiate map.
 function initMap() {
 	var mapOptions = {
 		center: centre,
@@ -14,11 +22,14 @@ function initMap() {
 
 	map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
+
+	// UN-FINISHED IMPLEMENTATION OF LOADING GOOGLE MARKERS FROM XML
 	// XML Parser.
 	// try: https://codepen.io/adrianparr/pen/cjlrn
 
 	// CAN'T GET AJAX METHOD TO SUCCEED...see above..
 	// Load the XML file.
+	/*
 	$.ajax({
 		type: "GET",
 		async: true,
@@ -46,4 +57,60 @@ function initMap() {
 			console.log("AJAX FAILED");
 		}
 	});
+	*/
 }
+
+// Add ibis static markers.
+function addIbisMarker() {
+	// Ibis Hotel marker.
+	ibisMarker = new google.maps.Marker({
+		position: ibisLatLng,
+		title:"Ibis Hotel"
+	});
+	// Set Ibis marker to map.
+	ibisMarker.setMap(map);
+
+	// Change map zoom and re-center.
+	map.setZoom(15);
+	map.setCenter(ibisLatLng);
+}
+
+// On button click, clear all markers then show Ibis marker.
+$('#mapIbis-btn').on('click', function() {
+    addIbisMarker()
+    mayfairMarker.setMap(null);
+})
+
+// Add mayfair static markers.
+function addMayfairMarker() {
+	// Mayfair Hotel marker.
+	mayfairMarker = new google.maps.Marker({
+		position: mayfairLatLng,
+		title:"Mayfair Hotel"
+	});
+	// Set mayfair marker to map.
+	mayfairMarker.setMap(map);
+
+	// Change map zoom and re-center.
+	map.setZoom(15);
+	map.setCenter(mayfairLatLng);
+}
+
+// On button click, clear markers then show mayfair markers.
+$('#mapMayfair-btn').on('click', function() {
+    addMayfairMarker()
+    ibisMarker.setMap(null);
+})
+
+// Add all static markers.
+function addAllMarkers() {
+	// Set ibis marker to map.
+	addIbisMarker();
+	// Set mayfair marker to map.
+	addMayfairMarker();
+}
+
+// On button click, show all markers.
+$('#mapall-btn').on('click', function() {
+    addAllMarkers()
+})
--- public/javascripts/modal.js
+++ public/javascripts/modal.js
@@ -1,32 +1,25 @@
 // MODAL
-// Get the modal
-var modal = document.getElementById('showAll_mapModal');
 
-// Get the button that opens the modal
-var btn = document.getElementById("mapall-btn");
+var modal = document.getElementById('showAll_mapModal'); // Get the modal.
+var btns = document.querySelectorAll('.mapButton');  	 // Get the buttons that open the modal.
+var span = document.getElementsByClassName("close")[0];	 // Get the span element that closes the modal (the x in top corner).
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+// For all buttons with class "mapButton", open modal when clicked.
+[].forEach.call(btns, function(el) {
+  el.onclick = function() {
+      modal.style.display = "block";
+  }
+})
 
-// When the user clicks on the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
+// Closes modal.
 span.onclick = function() {
     modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
+ // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
-}
-
-// Closes modal.
-function closeWindow() {
-	modal.style.display = "none";
 }
 
