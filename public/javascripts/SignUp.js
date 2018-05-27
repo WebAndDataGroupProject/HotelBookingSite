7425c1709c2159fbe7810203cc3e8eed47b30dca
WDC18 <37322133+WDC18@users.noreply.github.com>
2018-05-28 01:15:28 +0930

    Mixing offline copy was working on for part 5

--- public/javascripts/SignUp.js
+++ public/javascripts/SignUp.js
@@ -13,5 +13,46 @@ function opentab(evt, parameter) {
     
 
 }
-// Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
+
+
+
+function dissapear_IFN(){
+	//Text dissappear
+	document.getElementById("Indfirstname").value="";
+}
+
+function dissapear_ILN(){
+	//Text dissappear
+	document.getElementById("Indlastname").value="";
+}
+
+function dissapear_IEM(){
+	//Text dissappear
+	document.getElementById("Indemail").value="";
+}
+
+
+
+function dissapear_CFN(){
+	//Text dissappear
+	document.getElementById("Cfirstname").value="";
+}
+
+function dissapear_CLN(){
+	//Text dissappear
+	document.getElementById("Clastname").value="";
+}
+
+function dissapear_CEM(){
+	//Text dissappear
+	document.getElementById("Cemail").value="";
+}
+
+
+function SubmitFormI(){
+
+}
+
+
+
+
